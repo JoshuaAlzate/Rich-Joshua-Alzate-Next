9d4e540871422e1e0368dd0cdde0a6a2d0371b57
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-02-13 23:11:15 +0800

    Fix responsive design of Profile Components

--- components/profile.tsx
+++ components/profile.tsx
@@ -3,12 +3,14 @@ import Image from "next/image";
 
 const Profile = () => {
     return (
-        <div className="min-h-screen flex items-center justify-center space-x-8">
-            <Image src={'/rja-profile-photo.jpg'} width={100} height={100}
-                className="rounded-full object-cover border-2" />
-            <div>
-                <h1 className="text-2xl">Rich Joshua Alzate</h1>
-                <h3 className="text-xl">Full-Stack Software Engineer</h3>
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="flex flex-col sm:flex-row items-center justify-center sm:space-x-8 space-y-8 sm:space-y-0">
+                <Image src={'/rja-profile-photo.jpg'} width={100} height={100}
+                    className="rounded-full object-cover border-2" />
+                <div>
+                    <h1 className="text-2xl">Rich Joshua Alzate</h1>
+                    <h3 className="text-xl">Full-Stack Software Engineer</h3>
+                </div>
             </div>
         </div>
     )
