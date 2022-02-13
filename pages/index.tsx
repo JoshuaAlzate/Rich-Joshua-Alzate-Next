114a2f0cda1ea280bfd199f4e1e44e54d18bfc74
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-02-13 23:10:22 +0800

    Remove unused imports in Home Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import Profile from '../components/profile'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
