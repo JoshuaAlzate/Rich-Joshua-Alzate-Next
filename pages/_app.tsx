55ffc23f8e7643729f25f1ca31d5ac41499d9d24
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-02-18 23:48:19 +0800

    Imported Swiper Autoplay module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
+import 'swiper/css';
 import type { AppProps } from 'next/app'
+import Swiper, { Autoplay } from 'swiper';
+Swiper.use([Autoplay]);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
