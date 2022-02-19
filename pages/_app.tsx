7aec6b8c294af3fe1d8126fcac2b241f995ed199
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-02-19 10:13:49 +0800

    Remove Swiper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,5 @@
 import '../styles/globals.css'
-import 'swiper/css';
 import type { AppProps } from 'next/app'
-import Swiper, { Autoplay } from 'swiper';
-Swiper.use([Autoplay]);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
